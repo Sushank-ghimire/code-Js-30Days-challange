b5d0f671131dc5ae227e87b533be03e7b907f127
ghimiresk <ghimiresushank08@gmail.com>
2024-07-13 16:19:35 -0700

    Day first completed of javascript thirty days challange of Chai Aur Code

---
+++ Day-1/index.js
@@ -0,0 +1,27 @@
+// Variable Declaration 
+var work = "Learning Javascript From Chai Aur Code";
+console.log(work);
+
+// Constant Declaration
+const what_to_do = "Learn Code";
+console.log(what_to_do);
+// what_to_do = "throw an error";
+
+// Different Data Types
+let name = "This is string";
+let num = 12;
+let is_working = true;
+
+// Reasigned new value inside a variable
+name = "Ramesh Roy";
+console.log(name, num, is_working);
+
+// Primitive Data Types
+let array = [1, 2, 3, 5];
+let object = {
+    name: "ram",
+    work: "Student",
+    age: 19,
+    is_hardworking: true
+};
+console.log(array, object);
---
+++ Day-1/readme.md
@@ -0,0 +1,11 @@
+
+# Variables In Javascript
+- Declared variable using var, const and let and observed their behaviour and scope of the variable
+- Also reasigned the previous declared variables
+
+# Data Types in Javascript
+- Number
+- String
+- Boolean
+- Object
+- Array
